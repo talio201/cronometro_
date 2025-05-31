4e6a17bb20ac50cc8142a3f6410dbac626c58bbd
talio201 <talio_fsa@hotmail.com>
2025-05-31 17:01:21 -0300

    first commit

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'cronometro-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/sounds/click.mp3',
+  '/sounds/start.mp3',
+  '/sounds/stop.mp3'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
